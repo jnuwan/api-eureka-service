7d370d16b247acd601f2aacee347025d41151c07
Nuwan Jayarathna <jnuwan2011@gmail.com>
2024-12-18 19:01:30 +0100

    main: Init Eureka service repo.

---
+++ src/test/java/com/microservice/api_eureka_service/ApiEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.api_eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
